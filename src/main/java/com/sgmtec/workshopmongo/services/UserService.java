40db5241be1acf572af8873b4228db485b3420d4
Silvio173 <silvio.sgmtec@gmail.com>
2021-06-24 20:28:27 -0300

    Obtendo um usuário por id

--- src/main/java/com/sgmtec/workshopmongo/services/UserService.java
+++ src/main/java/com/sgmtec/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.sgmtec.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sgmtec.workshopmongo.domain.User;
 import com.sgmtec.workshopmongo.repository.UserRepository;
+import com.sgmtec.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Id não encontrado")); 
 	}
 }
---
+++ src/main/java/com/sgmtec/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,12 @@
+package com.sgmtec.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
