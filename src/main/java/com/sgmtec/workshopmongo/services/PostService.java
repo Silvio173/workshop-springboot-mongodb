41f6652291a27c31261b2c12a07f084506411c7c
Silvio173 <silvio.sgmtec@gmail.com>
2021-06-25 04:58:35 -0300

    Operação de instanciação da base de dados ERRO ARRUMADO

--- src/main/java/com/sgmtec/workshopmongo/services/PostService.java
+++ src/main/java/com/sgmtec/workshopmongo/services/PostService.java
@@ -27,6 +27,6 @@ public class PostService {
 	}
 	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
 		 maxDate = new Date(maxDate.getTime() + 24 * 60 *60 *1000);
-			return repo.allSearch(text);
+			return repo.fullSearch(text,minDate,maxDate);
 		}
 }
