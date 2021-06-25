1ec98f91d45d76fd8e88f9b44cd5fb59b0d292b7
Silvio173 <silvio.sgmtec@gmail.com>
2021-06-25 04:45:58 -0300

    Consulta com vários critérios

--- src/main/java/com/sgmtec/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/sgmtec/workshopmongo/repository/PostRepository.java
@@ -13,6 +13,8 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	
 	@Query( "{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> allSearch(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/sgmtec/workshopmongo/resource/PostResource.java
+++ src/main/java/com/sgmtec/workshopmongo/resource/PostResource.java
@@ -1,6 +1,6 @@
 package com.sgmtec.workshopmongo.resource;
 
-import java.net.URLDecoder;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +31,19 @@ public class PostResource {
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String Text) {
 		Text = URL.decodeParam(Text);
 		List<Post> list = service.findByTitle(Text);
+		return ResponseEntity.ok().body(list);
+	}
+
+	@RequestMapping(value = "/fullSearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value="text", defaultValue = "") String Text,
+	        @RequestParam(value="minDate", defaultValue = "") String minDate,
+            @RequestParam(value="maxDate", defaultValue = "") String maxDate){
+		Text = URL.decodeParam(Text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		
+		List<Post> list = service.fullSearch(Text,min,max);
 		return ResponseEntity.ok().body(list);
 	}
 }
